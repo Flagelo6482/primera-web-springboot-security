08213d5f60dd943912d4e62d2316d53dfbaabc3d
Frank <jesusc6482@gmail.com>
2024-10-03 18:45:33 -0500

    seguridad con JDBC

---
+++ src/main/java/com/tcna/primeraweb/entities/User.java
@@ -0,0 +1,21 @@
+package com.tcna.primeraweb.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String username;
+    private String password;
+    private boolean enable;
+    private String role;
+}
--- src/main/java/com/tcna/primeraweb/security/WebSecurityConfig.java
+++ src/main/java/com/tcna/primeraweb/security/WebSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.tcna.primeraweb.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,15 +15,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 @Configuration //Registra beans
-@EnableWebSecurity
+@EnableWebSecurity //Para habilitar la seguridad
 public class WebSecurityConfig {
 
+    /*
     @Bean //Para registrar este codificador en la fabrica de SPRING
     public PasswordEncoder passwordEncoder(){
 
         return new BCryptPasswordEncoder();
     }
+
 
     @Bean //Registra un bean en el contenedor de spring
     public InMemoryUserDetailsManager userDetailsManager() {
@@ -38,6 +45,24 @@ public class WebSecurityConfig {
                 .build();
         return new InMemoryUserDetailsManager(user1, user2);
     }
+     */
+
+    //Nos indica que la seguridad de spring boot trabaje con una base de datos
+    @Autowired
+    private DataSource dataSource;
+
+    /*
+    * Método para configurar todo el proceso de autenticación, basada en JDBC, donde obtiene los nombres, datos de una
+    * base de datos, para ellos necesitamos el codificador de bycript
+    * */
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder builder) throws Exception {
+        builder.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .dataSource(dataSource)
+                //Como vamos a obtener los username de el "dataSource" que apunta a nuestra base de datos, para cargar los usuarios
+                .usersByUsernameQuery("select username, password, enabled from users where username=?")
+                .authoritiesByUsernameQuery("select username, role from users where username=?"); //Obtenemos los roles
+    }
 
 
     //Filtro!!!, encargado de la parte de autorización y autenticación
@@ -48,9 +73,9 @@ public class WebSecurityConfig {
         //Indicamos que endpoints vamos a autorizar
         httpSecurity.authorizeHttpRequests(
                 auth -> auth
-                        .requestMatchers("/personas").permitAll() //Este endpoint será disponible para todos los usuarios que ingresen a la página
-                        .requestMatchers("/personas/nueva").hasAnyRole("ADMIN") //Solo a las personas que tengan el rol de ADMIN tienen acceso a este endpoint
-                        .requestMatchers("/personas/editar/*","/personas/eliminar/*").hasRole("ADMIN") //El "*" es un parametro, donde el ADMIN tiene acceso
+                        .requestMatchers("/animales").permitAll() //Este endpoint será disponible para todos los usuarios que ingresen a la página
+                        .requestMatchers("/animales/nueva").hasAnyRole("ADMIN") //Solo a las personas que tengan el rol de ADMIN tienen acceso a este endpoint
+                        .requestMatchers("/animales/editar/*","/animales/eliminar/*").hasRole("ADMIN") //El "*" es un parametro, donde el ADMIN tiene acceso
                         .anyRequest().authenticated()) //Cualquier otra petición que hagamos estara autenticada
                 .formLogin(form -> form     //Indicamos el formulario para logearnos
                         .loginPage("/login")
